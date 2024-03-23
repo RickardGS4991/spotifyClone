deda1cf29af0e3b8592842c7437e380f3d581173
ritchie928 <rickard928@gmail.com>
2024-03-23 03:46:38 -0600

    New component authentication

---
+++ providers/ToasterProvider.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { Toaster } from "react-hot-toast";
+
+const ToasterProvider = () => {
+    return (
+        <div>
+            <Toaster toastOptions={{
+                style: {
+                    background:'#333',
+                    color: '#fff'
+                }
+            }}/>
+        </div>
+    )
+}
+
+export default ToasterProvider;
