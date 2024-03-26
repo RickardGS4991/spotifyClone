feb648c04b8007e3a721a9eb26f00677a825daaa
ritchie928 <rickard928@gmail.com>
2024-03-25 18:56:26 -0600

    imagePath added

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,13 +1,12 @@
+import getSongs from "@/actions/getSongs";
 import Header from "@/components/Header";
-import Image from "next/image";
 import ListItem from "@/components/ListItem";
+import PageContent from "./components/PageContent";
 
-interface HeaderProps {
-  children: React.ReactNode;
-  className?: string;
-}
+export const revalidate = 0;
 
-export default function Home() {
+export default async function Home() {
+  const songs = await getSongs();
   return (
     <div className="
     bg-neutral-900
@@ -35,7 +34,9 @@ export default function Home() {
       </Header>
       <div className="mt-2 mb-7 px-6">
       <div className="flex justify-between items-center">
-      <h1 className="text-white text-2xl font-semibold">Newest Song</h1>
+      <h1 className="text-white text-2xl font-semibold">
+        <PageContent songs={songs}/>
+      </h1>
       </div>
       </div>
     </div>
